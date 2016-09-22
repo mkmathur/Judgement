57f489984b9ffb55274ef4a24e100bf8886d1fee
Mallika Mathur <mallika@mkmathur.com>
2016-09-21 18:10:08 -0700

    Add intro view

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const IntroView = () => (
+  <div className="IntroView">
+    <div className="IntroView-header">
+      <h2>Judgement</h2>
+    </div>
+    <button>Create Game</button>
+    <button>Join Game</button>
+  </div>
+);
+
+const App = () => (
+  <IntroView />    
+);
 
 export default App;
