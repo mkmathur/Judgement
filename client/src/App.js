c6b728c6eba56e4bad18b756d528fd8bcd98e1c3
Mallika Mathur <mallika@mkmathur.com>
2016-09-21 19:06:12 -0700

    Add views for new game, join game

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,53 @@
 import React from 'react';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, Link, browserHistory} from 'react-router';
 import './App.css';
 
-const IntroView = () => (
-  <div className="IntroView">
-    <div className="IntroView-header">
-      <h2>Judgement</h2>
-    </div>
-    <button>Create Game</button>
-    <button>Join Game</button>
+const Header = () => (
+  <div className="Header">
+    <h2>Judgement</h2>
+  </div>
+)
+
+const Intro = () => (
+  <div className="Intro">
+    <Header />
+    <Link to="/new">
+      <button>New Game</button>
+    </Link>
+    <Link to="/join">
+      <button>Join Game</button>
+    </Link>
+  </div>
+);
+
+const NewGame = () => (
+  <div className="NewGame">
+    <Header />
+    <input type="text" placeholder="Enter your name" />
+    <button>Create</button>
+    <Link to="/">
+      <button>Back</button>
+    </Link>
+  </div>
+);
+
+const JoinGame = () => (
+  <div className="JoinGame">
+    <Header />
+    <input type="text" placeholder="Enter an access code" />
+    <input type="text" placeholder="Enter your name" />
+    <button>Join</button>
+    <Link to="/">
+      <button>Back</button>
+    </Link>
   </div>
 );
 
 const App = () => (
   <Router history={browserHistory}>
-    <Route path="/" component={IntroView} />
+    <Route path="/" component={Intro} />
+    <Route path="/new" component={NewGame} />
+    <Route path="/join" component={JoinGame} />
   </Router>
 );
 
