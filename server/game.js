14f239105ed00a5a574a5f4ce3e13a78bc3168ea
Mallika Mathur <mallika@mkmathur.com>
2016-09-23 15:34:51 -0700

    Add comments on game methods

--- server/game.js
+++ server/game.js
@@ -1,5 +1,10 @@
 const DECK_SIZE = 52;
 
+// const card = {
+//   suit: "Spades",
+//   rank: "Q"
+// };
+
 export const STATES = {
   WAITING_FOR_PLAYERS: 0,
   STARTING_GAME: 1,
@@ -8,26 +13,28 @@ export const STATES = {
   GAME_OVER: 4,
 };
 
-export function maxRoundNumber(numPlayers) {
-  return Math.floor(DECK_SIZE / numPlayers);
-}
-
-export function nextRoundNumber(num, max) {
+// returns a list a, where a[i] is the number of cards in each hand in round i
+// for 4 players, should return: [1, 2, 3, ... , 12, 13, 12, ..., 2, 1]
+export function rounds(numPlayers) {
 
 }
 
-export function nextTrump(suit) {
+export function nextTrump(currentTrump) {
 
 }
 
-export function nextPlayer(playerID) {
+export function nextPlayer(currentPlayerIndex, numPlayers) {
 
 }
 
+// hand is a list of cards
+// table is a list of cards, indexed by player id
+// i.e. table[0] = {'Q', 'Diamonds'} means that player 0 put down the Queen of Diamonds
 export function validateCard(card, hand, table, chaal) {
 
 }
 
+// return id of the player who wins the trick
 export function determineTrickWinner(table, chaal, trump) {
 
 }
